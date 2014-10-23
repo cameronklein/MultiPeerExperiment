d98d95645faf271990e3378c4f000e5932c96592
Cameron Klein <cameron.klein@gmail.com>
2014-10-23 13:06:40 -0700

    initial commit

--- MultiPeer Experiment/ViewController.swift
+++ MultiPeer Experiment/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MCSessionDelegate, MCNearbyServiceAdvertiserDelegate{
+  
+  var peerID : MCPeerID!
+  var session : MCSession!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    peerID =  MCPeerID(displayName: UIDevice.currentDevice().name)
+    session = MCSession(peer: peerID)
+    
+    self.session.delegate = self
+
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!) {
+    println("Got Resource")
   }
-
-
+  
+  func session(session: MCSession!, didReceiveData data: NSData!, fromPeer peerID: MCPeerID!) {
+    println("Received Data!")
+  }
+  
+  func session(session: MCSession!, didReceiveStream stream: NSInputStream!, withName streamName: String!, fromPeer peerID: MCPeerID!) {
+    println("Received Stream!")
+  }
+  
+  func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!) {
+    println("Started Receiving Resource")
+  }
+  
+  func session(session: MCSession!, peer peerID: MCPeerID!, didChangeState state: MCSessionState) {
+    println("Changed State")
+  }
+  
+  func advertiser(advertiser: MCNearbyServiceAdvertiser!, didReceiveInvitationFromPeer peerID: MCPeerID!, withContext context: NSData!, invitationHandler: ((Bool, MCSession!) -> Void)!) {
+    
+    var didAccept = false
+    let alert = UIAlertController(title: "You have been invited by \(peerID)", message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
+    let ok = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) { (action) -> Void in
+      didAccept = true
+    }
+    let no = UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel) { (action) -> Void in
+      alert.dismissViewControllerAnimated(true, completion: nil)
+    }
+    alert.addAction(ok)
+    alert.addAction(no)
+    self.presentViewController(alert, animated: true, completion: nil)
+    
+    let session = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: MCEncryptionPreference.None)
+    session.delegate = self
+    
+    invitationHandler(didAccept, session)
+    
+    }
+  
+  @IBAction func didPressButton(sender: AnyObject) {
+    println("Did Press Button")
+    let XXServiceType = "xx-service"
+    
+    let advertiser = MCNearbyServiceAdvertiser(peer: peerID, discoveryInfo: nil, serviceType: XXServiceType)
+    advertiser.delegate = self
+    advertiser.startAdvertisingPeer()
+    
+  }
+  
+  
 }
+  
+//  func browserViewControllerDidFinish(browserViewController: MCBrowserViewController!) {
+//    self.dismissViewControllerAnimated(true, completion: nil)
+//  }
+//  
+//  func browserViewControllerWasCancelled(browserViewController: MCBrowserViewController!) {
+//    self.dismissViewControllerAnimated(true, completion: nil)
+//  }
+
 
